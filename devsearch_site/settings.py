59c594f1117eeaeb06d0422b3e2c2e2fdef8766f
avjasso <avjasso@gmail.com>
2021-09-05 23:17:51 -0500

    Create and register projects app

--- devsearch_site/settings.py
+++ devsearch_site/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'projects.apps.ProjectsConfig',
 ]
 
 MIDDLEWARE = [
