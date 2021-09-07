f2264513c4c440ad9671ae3d6ddc4ca23506360e
avjasso <avjasso@gmail.com>
2021-09-07 01:35:29 -0500

    Add templates folder, basic project routing, projects model

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def projects(request):
+    msg = "Hello, you are on the projects page"
+    return render(request, 'projects/projects.html', {'message': msg})
+
+
+def project(request, pk):
+    return render(request, 'projects/single-project.html', {'pk': pk})
